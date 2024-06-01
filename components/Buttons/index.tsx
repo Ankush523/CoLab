9cedd1d762974fe33daf81e7d533eeca88a71c6b
Ankush523 <ankush.ad050203@gmail.com>
2024-06-01 15:38:07 +0530

    huddle room link

--- components/Buttons/index.tsx
+++ components/Buttons/index.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
   const openNewTab = () => {
-    window.open("http://localhost:3001/gpa-vzfj-kpz", "_blank");
+    window.open("https://colab-huddle.vercel.app/gpa-vzfj-kpz", "_blank");
   };
 
   return <button className="bg-indigo-500 px-4 rounded-full text-white shadow-xl" onClick={openNewTab}>Join Meeting</button>;
